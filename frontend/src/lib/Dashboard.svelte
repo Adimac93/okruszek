305341f66bfc954a55248838cc2ba8edd17323f3
Adimac93 <adimac93@gmail.com>
2023-03-19 12:52:49 +0100

    🚑️ remove unused imports

--- frontend/src/lib/Dashboard.svelte
+++ frontend/src/lib/Dashboard.svelte
@@ -1,8 +1,6 @@
 <script lang="ts">
   import AddProduct from "./AddProduct.svelte";
   import Logout from "./auth/Logout.svelte";
-  import Load from "./files/Load.svelte";
-  import Upload from "./files/Upload.svelte";
   import Products from "./Products.svelte";
 </script>
 
